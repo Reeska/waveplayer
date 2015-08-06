7b70f37d7cfdb9a88b356562f5f14ec7448b41ae
Reeska <thchampi@gmail.com>
2015-08-06 19:00:44 +0200

    Add fix for IE8 in https : using embed tag

--- dist/waveplayer.js
+++ dist/waveplayer.js
@@ -1,19 +1,46 @@
 function WavePlayer(container) {
-	this.ie = !!document.documentMode;
+	this.container = container || 'body';
+	this.ie = document.documentMode;
 	this.playing = false;
-	this.$bgsound = $('<bgsound volume="3" balance="0"/>').appendTo(container || 'body');
 	
-	if ('Audio' in window)
-		this.audioPlayer = new Audio();
+	this.$bgsound = this.ie > 8 ? $('<bgsound volume="3" balance="0"/>').appendTo(this.container) : null;
+	this.audioPlayer = 'Audio' in window ? new Audio() : null
 };
 
+WavePlayer.prototype.supported = function() {
+	return this.ie || this.audioPlayer;
+};
 
 WavePlayer.prototype.play = function(uri) {
-	if (this.ie)
+	/**
+	 * Use for IE9+ : .wav file not supported for audio and embed tag
+	 */
+	if (this.ie > 8) {
 		this.$bgsound.attr('src', uri);
-	else {
+	} 
+	/**
+	 * For modern browsers
+	 */
+	else if (this.audioPlayer) {
 		this.audioPlayer.src = uri;
 		this.audioPlayer.play();
+	} 
+	/**
+	 * Other browsers and IE8: Use embed because there is an error with bgsound and https for IE8
+	 */
+	else {
+		/*
+		 * remove embed if uri changes
+		 */
+		if (this.embed && this.embed[0].src != uri) {
+			this.embed.remove();
+			this.embed = null;
+		}
+		
+		if (this.embed)
+			this.embed[0].Play();
+		else
+			this.embed = $('<embed height="0" src="' + uri +'" type="audio/wav"/>').appendTo(this.container);
 	}
 	
 	this.playing = true;
@@ -22,12 +49,14 @@ WavePlayer.prototype.play = function(uri) {
 };
 
 WavePlayer.prototype.stop = function() {
-	if (this.ie)
+	if (this.ie > 8) {
 		this.$bgsound.attr('src', '');
-	else {
+	} else if (this.audioPlayer) {
 		this.audioPlayer.src = '';
 		this.audioPlayer.pause();
-	}	
+	} else if (this.embed) {
+		this.embed[0].Stop();		
+	}
 	
 	this.playing = false;
 	return this;
